aa60bf82d05a9fa680529fa6c6b1a9f703b002c0
keisuke_ubuntu <57240541+keisukeh1016@users.noreply.github.com>
2020-06-07 18:01:50 +0900

    銘柄検索機能を追加

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,9 +1,16 @@
 class StocksController < ApplicationController
   def index
-    @stocks = Stock.order(dod_change: :desc)
+    @q = Stock.ransack(params[:q])
+    @stocks = @q.result(distinct: true).order(dod_change: :desc).limit(15)
   end
 
   def show
     @stock = Stock.find(params[:id])
   end
+
+  private
+
+    def search_params
+      params.require(:q).permit(:code_eq, :name_cont)
+    end
 end
