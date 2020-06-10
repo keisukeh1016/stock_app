bce98e4870705644bb48281f00cbe517de5d318e
keisuke_ubuntu <57240541+keisukeh1016@users.noreply.github.com>
2020-06-10 15:39:46 +0900

    fix bug about mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,14 +62,14 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'stock-ja.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: ENV['SERVER_HOST'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name:            ENV['SENDGRID_USERNAME'],
     password:             ENV['SENDGRID_PASSWORD'],
     address:              'smtp.sendgrid.net',
     port:                 587,
-    domain:               'herokuapp.com',
+    domain:               'google.com',
     authentication:       :plain,
     enable_starttls_auto: true
   }
